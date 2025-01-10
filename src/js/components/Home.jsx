f9509361ca988ba32435ef5fca0a63f9afc700bd
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-01-10 17:20:12 +0000

    Traffic Light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,27 +3,28 @@ import React, { useState } from "react";
 
 const Home = () => {
 
-	const [selected, setSelected]= useState(null);
+	
+	const [color, setColor]= useState(null);
 	const changeColor = () => {
-		if (selected === "red") {
-		  setSelected("green");
-		} else if (selected === "green") {
-		  setSelected("yellow");
+		if (color === "red") {
+		  setColor("green");
+		} else if (color === "green") {
+		  setColor("yellow");
 		} else {
-		  setSelected("red"); 
+		  setColor("red"); 
 		}
 	  };
 	  
 	function stop(){
-			setSelected("red");
+			setColor("red");
 		}
 	
 	function attention(){
-			setSelected("yellow")
+			setColor("yellow")
 		}
 	
 	function play(){
-			setSelected("green")
+			setColor("green")
 		
 	}
 
@@ -33,9 +34,9 @@ const Home = () => {
 			<div className="traffic-light-container" style={{  display: "flex", flexDirection: "column", alignItems: "center",}}>
 				<div className="palo" style={{ width: "20px", height:"100px", backgroundColor: "black", marginBottom: "-10px", }}></div>
 				<div  style={{ display: "flex", flexDirection: "column", alignItems: "center", backgroundColor: "black", width: "120px", padding: "20px", borderRadius: "10px",}}>
-					<div onClick={stop} style={{ width: "60px", height: "60px", borderRadius: "50%", backgroundColor: "red", boxShadow: selected === "red" ? "0 0 20px 10px red" : "none", margin: "10px", cursor: "pointer",}}></div>
-					<div onClick={attention} style={{ width: "60px", height: "60px", borderRadius: "50%", backgroundColor: "yellow", boxShadow: selected === "yellow" ? "0 0 20px 10px yellow" : "none", margin: "10px", cursor: "pointer",}}></div>
-					<div onClick={play} style={{ width: "60px",  height: "60px", borderRadius: "50%", backgroundColor: "green", boxShadow: selected === "green" ? "0 0 20px 10px green" : "none", margin: "10px", cursor: "pointer",}}></div>
+					<div onClick={stop} style={{ width: "60px", height: "60px", borderRadius: "50%", backgroundColor: "red", boxShadow: color === "red" ? "0 0 20px 10px red" : "none", margin: "10px", cursor: "pointer",}}></div>
+					<div onClick={attention} style={{ width: "60px", height: "60px", borderRadius: "50%", backgroundColor: "yellow", boxShadow: color === "yellow" ? "0 0 20px 10px yellow" : "none", margin: "10px", cursor: "pointer",}}></div>
+					<div onClick={play} style={{ width: "60px",  height: "60px", borderRadius: "50%", backgroundColor: "green", boxShadow: color === "green" ? "0 0 20px 10px green" : "none", margin: "10px", cursor: "pointer",}}></div>
 				</div>
 				<button  onClick={changeColor} style={{ marginTop: "20px", padding: "10px 20px", fontSize: "20px", borderRadius:"10px", backgroundColor:"blue", color:"white", cursor: "pointer", }}>Alternar Color</button>
 			</div>
